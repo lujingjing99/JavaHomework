339c59b499e8ebaf9cf511edc9ac10c5a557ed25
lujingjing99 <54838034+lujingjing99@users.noreply.github.com>
2019-10-16 21:03:46 +0800

    Add files via upload

---
+++ weather/src/net/sf/json/JSONObject.java
@@ -0,0 +1,20 @@
+package net.sf.json;
+
+public class JSONObject {
+
+	public static JSONObject fromObject(String datas) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public JSONObject getJSONObject(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getString(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
